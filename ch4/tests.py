0aaf5e33e9330cd90059d7a5b4ef1e7d15eba7f0
vikas <vikasrao416@gmail.com>
2021-12-06 15:44:43 +0530

    add test

--- ch4/tests.py
+++ ch4/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from ch4.models import Page
+from django.urls import reverse
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self):
+        Page.objects.create(text='just a game')
+    def test_of_model(self):
+        page = Page.objects.get(id=1)
+        expected_outcome = f'{page.text}'
+        self.assertEqual(expected_outcome,'just a game')
+class HomepageTest(TestCase):
+    def setUp(self):
+        Page.objects.create(text='another test')
+    def test_url_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code,200)
+    def test_url_byName(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+    def test_url_html(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+        self.assertTemplateUsed(resp,'ch4/home_list.html')
